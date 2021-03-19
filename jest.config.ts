a4e348d970d0f16f64f81025469c2daf3a5fb65d
Vlad Frangu <kingdgrizzle@gmail.com>
2021-03-19 12:16:20 +0200

    feat: TypeScript conversion base

---
+++ jest.config.ts
@@ -0,0 +1,28 @@
+import { join } from 'path';
+import type { Config } from '@jest/types';
+
+export default async (): Promise<Config.InitialOptions> => ({
+    verbose: true,
+    // Switch to false for debugging
+    silent: true,
+    // rootDir: join(__dirname, './'),
+    // testMatch: [join(__dirname, '**/test/?(*.)+(spec|test).[tj]s?(x)')],
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    testRunner: 'jest-circus/runner',
+    testTimeout: 5000,
+    collectCoverage: true,
+    collectCoverageFrom: [
+        '**/src/**/*.ts',
+        '**/src/**/*.js',
+        '!**/node_modules/**',
+    ],
+    maxWorkers: 3,
+    globalSetup: join(__dirname, 'test', '_globalSetup.js'),
+    globalTeardown: join(__dirname, 'test', '_globalTeardown.js'),
+    globals: {
+        'ts-jest': {
+            tsconfig: '<rootDir>/test/tsconfig.json',
+        },
+    },
+});
