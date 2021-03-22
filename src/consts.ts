d4fca190dba244728662b7644c719ff6a907c523
Vlad Frangu <kingdgrizzle@gmail.com>
2021-03-22 15:02:45 +0200

    fix: move objects to enum

--- src/consts.ts
+++ src/consts.ts
@@ -11,20 +11,20 @@ export const TIMESTAMP_SQL = "STRFTIME('%Y-%m-%dT%H:%M:%fZ', 'NOW')";
 /**
  * Types of all emulated storages (currently used for warning messages only).
  */
-export const STORAGE_TYPES = {
-    REQUEST_QUEUE: 'Request queue',
-    KEY_VALUE_STORE: 'Key-value store',
-    DATASET: 'Dataset',
-} as const;
+export const enum STORAGE_TYPES {
+    REQUEST_QUEUE = 'Request queue',
+    KEY_VALUE_STORE = 'Key-value store',
+    DATASET = 'Dataset',
+};
 
 /**
  * Names of all emulated storages.
  */
-export const STORAGE_NAMES = {
-    REQUEST_QUEUES: 'request_queues',
-    KEY_VALUE_STORES: 'key_value_stores',
-    DATASETS: 'datasets',
-} as const;
+export const enum STORAGE_NAMES {
+    REQUEST_QUEUES = 'request_queues',
+    KEY_VALUE_STORES = 'key_value_stores',
+    DATASETS = 'datasets',
+};
 
 /**
  * Name of the request queue master database file.
